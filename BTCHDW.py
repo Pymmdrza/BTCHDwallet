1f117842cf8bc77e03c9be4255fb2db84fb2ef06
MMDRZA <95309780+Pymmdrza@users.noreply.github.com>
2022-03-15 14:40:59 +0300

    Bitcoin Wallet Private Key And All Address Type
    Bitcoin Wallet Private Key And All Address Type Generated With HDWallet Package

---
+++ BTCHDW.py
@@ -0,0 +1,158 @@
+mmdrza = '''
+=========================[M M D Z A . C o M]==============================
+             _ __ ___  _ __ ___   __| |_ __ ______ _
+            | '_ ` _ \| '_ ` _ \ / _` | '__|_  / _` |
+            | | | | | | | | | | | (_| | |   / / (_| |
+            |_| |_| |_|_| |_| |_|\__,_|_|  /___\__,_|
+=========================[M M D Z A . C o M]==============================
+= Athur : MMDRZA
+= Email : x4@Mmdrza.Com
+= Web : https://Mmdrza.Com
+= Dev.to/mmdrza
+= Github.Com/Pymmdrza
+=========================[M M D Z A . C o M]==============================
+= Donat = Bitcoin 16p9y6EstGYcnofGNvUJMEGKiAWhAr1uR8
+=========================[M M D Z A . C o M]==============================
+        '''
+from hdwallet import HDWallet
+from hdwallet.symbols import ETH as SYMBOL
+import random
+from colorama import Fore, Style
+from lxml import html
+import requests
+green = Fore.GREEN
+red = Fore.RED
+white = Fore.WHITE
+yellow = Fore.YELLOW
+reset = Style.RESET_ALL
+# =========================[M M D Z A . C o M]==============================
+print(Fore.GREEN,mmdrza,Style.RESET_ALL)
+print('Start ...')
+time.sleep(5)
+# =========================[M M D Z A . C o M]==============================
+z = 1
+while True:
+    c1 = str(random.choice('0123456789abcdefABCDEF'))
+    c2 = str(random.choice('0123456789abcdefABCDEF'))
+    c3 = str(random.choice('0123456789abcdefABCDEF'))
+    c4 = str(random.choice('0123456789abcdefABCDEF'))
+    c5 = str(random.choice('0123456789abcdefABCDEF'))
+    c6 = str(random.choice('0123456789abcdefABCDEF'))
+    c7 = str(random.choice('0123456789abcdefABCDEF'))
+    c8 = str(random.choice('0123456789abcdefABCDEF'))
+    c9 = str(random.choice('0123456789abcdefABCDEF'))
+    c10 = str(random.choice('0123456789abcdefABCDEF'))
+    c11 = str(random.choice('0123456789abcdefABCDEF'))
+    c12 = str(random.choice('0123456789abcdefABCDEF'))
+    c13 = str(random.choice('0123456789abcdefABCDEF'))
+    c14 = str(random.choice('0123456789abcdefABCDEF'))
+    c15 = str(random.choice('0123456789abcdefABCDEF'))
+    c16 = str(random.choice('0123456789abcdefABCDEF'))
+    c17 = str(random.choice('0123456789abcdefABCDEF'))
+    c18 = str(random.choice('0123456789abcdefABCDEF'))
+    c19 = str(random.choice('0123456789abcdefABCDEF'))
+    c20 = str(random.choice('0123456789abcdefABCDEF'))
+    c21 = str(random.choice('0123456789abcdefABCDEF'))
+    c22 = str(random.choice('0123456789abcdefABCDEF'))
+    c23 = str(random.choice('0123456789abcdefABCDEF'))
+    c24 = str(random.choice('0123456789abcdefABCDEF'))
+    c25 = str(random.choice('0123456789abcdefABCDEF'))
+    c26 = str(random.choice('0123456789abcdefABCDEF'))
+    c27 = str(random.choice('0123456789abcdefABCDEF'))
+    c28 = str(random.choice('0123456789abcdefABCDEF'))
+    c29 = str(random.choice('0123456789abcdefABCDEF'))
+    c30 = str(random.choice('0123456789abcdefABCDEF'))
+    c31 = str(random.choice('0123456789abcdefABCDEF'))
+    c32 = str(random.choice('0123456789abcdefABCDEF'))
+    c33 = str(random.choice('0123456789abcdefABCDEF'))
+    c34 = str(random.choice('0123456789abcdefABCDEF'))
+    c35 = str(random.choice('0123456789abcdefABCDEF'))
+    c36 = str(random.choice('0123456789abcdefABCDEF'))
+    c37 = str(random.choice('0123456789abcdefABCDEF'))
+    c38 = str(random.choice('0123456789abcdefABCDEF'))
+    c39 = str(random.choice('0123456789abcdefABCDEF'))
+    c40 = str(random.choice('0123456789abcdefABCDEF'))
+    c41 = str(random.choice('0123456789abcdefABCDEF'))
+    c42 = str(random.choice('0123456789abcdefABCDEF'))
+    c43 = str(random.choice('0123456789abcdefABCDEF'))
+    c44 = str(random.choice('0123456789abcdefABCDEF'))
+    c45 = str(random.choice('0123456789abcdefABCDEF'))
+    c46 = str(random.choice('0123456789abcdefABCDEF'))
+    c47 = str(random.choice('0123456789abcdefABCDEF'))
+    c48 = str(random.choice('0123456789abcdefABCDEF'))
+    c49 = str(random.choice('0123456789abcdefABCDEF'))
+    c50 = str(random.choice('0123456789abcdefABCDEF'))
+    c51 = str(random.choice('0123456789abcdefABCDEF'))
+    c52 = str(random.choice('0123456789abcdefABCDEF'))
+    c53 = str(random.choice('0123456789abcdefABCDEF'))
+    c54 = str(random.choice('0123456789abcdefABCDEF'))
+    c55 = str(random.choice('0123456789abcdefABCDEF'))
+    c56 = str(random.choice('0123456789abcdefABCDEF'))
+    c57 = str(random.choice('0123456789abcdefABCDEF'))
+    c58 = str(random.choice('0123456789abcdefABCDEF'))
+    c59 = str(random.choice('0123456789abcdefABCDEF'))
+    c60 = str(random.choice('0123456789abcdefABCDEF'))
+    c61 = str(random.choice('0123456789abcdefABCDEF'))
+    c62 = str(random.choice('0123456789abcdefABCDEF'))
+    c63 = str(random.choice('0123456789abcdefABCDEF'))
+    c64 = str(random.choice('0123456789abcdefABCDEF'))
+    magic = (
+            c1 + c2 + c3 + c4 + c5 + c6 + c7 + c8 + c9 + c10 + c11 + c12 + c13 + c14 + c15 + c16 + c17 + c18 + c19 + c20 + c21 + c22 + c23 + c24 + c25 + c26 + c27 + c28 + c29 + c30 + c31 + c32 + c33 + c34 + c35 + c36 + c37 + c38 + c39 + c40 + c41 + c42 + c43 + c44 + c45 + c46 + c47 + c48 + c49 + c50 + c51 + c52 + c53 + c54 + c55 + c56 + c57 + c58 + c59 + c60 + c61 + c62 + c63 + c64)
+    PRIVATE_KEY = str(magic)
+    hdwallet: HDWallet = HDWallet(symbol=SYMBOL)
+    hdwallet.from_private_key(private_key=PRIVATE_KEY)
+    privatekey = hdwallet.private_key()
+    ethaddr = hdwallet.p2pkh_address()  # 0xEF3589b72bd2f2e539843aa0Af67FEd18906A761
+    btcaddr1 = hdwallet.p2sh_address()
+    btcaddr2 = hdwallet.p2wpkh_address()
+    btcaddr3 = hdwallet.p2wpkh_in_p2sh_address()
+    btcaddr4 = hdwallet.p2wsh_in_p2sh_address()
+    # =========================[M M D Z A . C o M]==============================
+    page4 = requests.get('https://bitcoin.atomicwallet.io/address/' + btcaddr4)
+    tree4 = html.fromstring(page4.content)
+    value4 = tree4.xpath('//small[@class="text-muted"]/text()')
+    # =========================[M M D Z A . C o M]==============================
+    page3 = requests.get('https://bitcoin.atomicwallet.io/address/' + btcaddr3)
+    tree3 = html.fromstring(page3.content)
+    value3 = tree3.xpath('//small[@class="text-muted"]/text()')
+    # =========================[M M D Z A . C o M]==============================
+    page2 = requests.get('https://bitcoin.atomicwallet.io/address/' + btcaddr2)
+    tree2 = html.fromstring(page2.content)
+    value2 = tree2.xpath('//small[@class="text-muted"]/text()')
+    # =========================[M M D Z A . C o M]==============================
+    page1 = requests.get('https://bitcoin.atomicwallet.io/address/' + btcaddr1)
+    tree1 = html.fromstring(page1.content)
+    value1 = tree1.xpath('//small[@class="text-muted"]/text()')
+    # =========================[M M D Z A . C o M]==============================
+    promptPUB = '''
+        *********************** Bitcoin HighSpeed Check Balance WithOut API ********************
+        *                                                                                      *
+        *    ** This Tools No Need API , Check Without API Balance Bitcoin Address             *
+        *    ** Create And Programmer Mmdrza Official Web Site https://mmdrza.com              *
+        *    ** ANY ADDRESS IF BALANCE high In 0 Save To This File (btcWin.txt)                *
+        *                                                                                      *
+        *********************** Enjoy Programming With M M D R Z A . C o M *********************
+        ========================================================================================
+            '''
+    print(yellow, promptPUB, reset)
+    print(red, '===========================[Scan No.', str(z), ']=====================================', reset)
+    print(green, '| BTC Address (P2SH):', str(btcaddr1))
+    print(green, '| BTC Address (BECH32):', str(btcaddr2))
+    print(white, '| BTC Address (P2WPKH):', str(btcaddr3))
+    print(white, '| BTC Address (P2WSH):', str(btcaddr4))
+    print(red, '| BTC Private Key :', str(privatekey))
+    print(red, '====================================[ M M D R Z A . C o M ]==========================================')
+    print(yellow, 'P2SH',white,str(value1), yellow,'BECH32',white,str(value2),yellow,'P2WPKH',white,str(value3),yellow,'P2WSH',white,str(value4))
+    print(red, '====================================[ M M D R Z A . C o M ]==========================================', reset)
+    z += 1
+		
+		
+		
+# =========================[M M D Z A . C o M]==============================
+# Programmer M M D R Z A 
+# Web Mmdrza.Com
+# Dev.to/Mmdrza
+# Github.Com/Pymmdrza
+# Donat = Bitcoin 16p9y6EstGYcnofGNvUJMEGKiAWhAr1uR8
+# =========================[M M D Z A . C o M]==============================
+    
