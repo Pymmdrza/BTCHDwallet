51efdb3561906ab39719be22c4efff690b14ef4d
MMDRZA <95309780+Pymmdrza@users.noreply.github.com>
2024-05-08 15:22:19 +0300

    Update btcSingle.bat

--- btcSingle.bat
+++ btcSingle.bat
@@ -3,6 +3,6 @@ title MMDRZA.COM - BTC & ETH HD Address
 Pushd "%~dp0"
 :loop
 pip install colorthon
-pip install hdwallet
+pip install hdwallet requests-random-user-agent
 python singleWallet.py
 goto loop
